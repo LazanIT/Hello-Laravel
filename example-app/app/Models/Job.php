ffea059c2fd1b13845c986132c45550021195dd7
Aleksandar Lazanski <aleksandar.lazanski3@gmail.com>
2025-06-27 20:35:32 +0200

    Unexpected status code & first Model

--- example-app/app/Models/Job.php
+++ example-app/app/Models/Job.php
@@ -2,7 +2,8 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
+
+use Illuminate\Support\Arr;
 
 class Job {
     public static function all(): array{
@@ -24,4 +25,16 @@ class Job {
             ]
             ];
     }
+
+    public static function find(int $id): array 
+    {
+
+         $job = Arr::first(static::all(), fn($job) => $job['id'] == $id);
+
+        if(! $job){
+            abort(404);
+        }
+        return $job;
+    }
 }
+
