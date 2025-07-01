af5135d6ef8fbef7e2dd0904763b056f234b7d2e
Aleksandar Lazanski <aleksandar.lazanski3@gmail.com>
2025-07-01 22:21:03 +0200

    Lazy Loading, Eager Loading and N+1 Problem

--- example-app/resources/views/jobs.blade.php
+++ example-app/resources/views/jobs.blade.php
@@ -6,7 +6,12 @@
     <div class="space-y-4">
         @foreach ($jobs as $job)
             <a href="/jobs/{{ $job['id'] }}" class="block px-4 py-6 border border-gray-200 rounded-lg">
-                <strong>{{ $job['title'] }}:</strong> Pays {{ $job['salary'] }}
+                <div class="font-bold text-blue-500 text-sm">
+                    {{ $job->employer->name }}
+                </div>
+                <div class="">
+                    <strong>{{ $job['title'] }}:</strong> Pays {{ $job['salary'] }}
+                </div>
         @endforeach
         </a>
     </div>
