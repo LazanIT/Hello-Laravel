cbd63c05e40e7f8d078b5f7191f406fe1d3fc7ab
Aleksandar Lazanski <aleksandar.lazanski3@gmail.com>
2025-07-03 23:53:26 +0200

    CSRF and Forms

--- example-app/resources/views/jobs/create.blade.php
+++ example-app/resources/views/jobs/create.blade.php
@@ -3,12 +3,14 @@
         Create Job
     </x-slot:heading>
     <form method="POST" action="/jobs">
+        @csrf
         <div class="space-y-12">
             <div class="border-b border-gray-900/10 pb-12">
                 <h2 class="text-base/7 font-semibold text-gray-900">Create a New Job</h2>
                 <p class="mt-1 text-sm/6 text-gray-600">We just need a handful of details</p>
 
                 <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+
                     <div class="sm:col-span-4">
                         <label for="title" class="block text-sm/6 font-medium text-gray-900">
                             Title
--- example-app/routes/web.php
+++ example-app/routes/web.php
@@ -16,7 +16,7 @@ Route::get('/', function () {
 
 Route::get('/jobs', function () {
 
-    $jobs = Job::with('employer')->simplePaginate(3);
+    $jobs = Job::with('employer')->latest()->simplePaginate(3);
 
      return view('jobs.index', [
         'jobs' => $jobs
@@ -36,7 +36,17 @@ Route::get('/jobs/{id}', function($id)  {
     return view('jobs.show', ['job' => $job]);
 });
 
+Route::post('/jobs', function(){
+    // validation....
 
+    Job::create([
+        'title' => request('title'),
+        'salary'=> request('salary'),
+        'employer_id' => 1
+    ]);
+ 
+    return redirect('/jobs');
+});
 
 Route::get('/contact', function(){
     return view('contact');
