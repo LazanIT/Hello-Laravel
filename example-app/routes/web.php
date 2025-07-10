0a6d86ee55b488ad543602c3f25985fa7e48d9c3
Aleksandar Lazanski <aleksandar.lazanski3@gmail.com>
2025-07-10 23:03:13 +0200

    Route Rosource

--- example-app/routes/web.php
+++ example-app/routes/web.php
@@ -6,23 +6,26 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::view('/', 'home');
- 
-Route::controller(JobController::class)->group(function () {
-    // Prikazuje sve poslove ( index )
-Route::get('/jobs', 'index');
-// Prikazuje formu za kreiranje posla ( create )
-Route::get('/jobs/create', 'create');
-// Prikazuje formu za prikazivanje odredjenog posla( show )
-Route::get('/jobs/{job}', 'show');
-// Prikazuje formu za cuvanje poslova ( store )
-Route::post('/jobs','store' );
-// Prikazuje formu za izmenu posla ( Edit )
-Route::get('jobs/{job}/edit', 'edit');
-// Azuriranje posla ( update )
-Route::patch('/jobs/{job}', 'update');
-// Brisanje posla ( delete )
-Route::delete('/jobs/{job}','destroy' );
-});
-
 
 Route::view('/contact', 'contact');
+ 
+// Route::controller(JobController::class)->group(function () {
+//     // Prikazuje sve poslove ( index )
+// Route::get('/jobs', 'index');
+// // Prikazuje formu za kreiranje posla ( create )
+// Route::get('/jobs/create', 'create');
+// // Prikazuje formu za prikazivanje odredjenog posla( show )
+// Route::get('/jobs/{job}', 'show');
+// // Prikazuje formu za cuvanje poslova ( store )
+// Route::post('/jobs','store' );
+// // Prikazuje formu za izmenu posla ( Edit )
+// Route::get('jobs/{job}/edit', 'edit');
+// // Azuriranje posla ( update )
+// Route::patch('/jobs/{job}', 'update');
+// // Brisanje posla ( delete )
+// Route::delete('/jobs/{job}','destroy' );
+// });
+
+
+Route::resource('jobs', JobController::class);
+
