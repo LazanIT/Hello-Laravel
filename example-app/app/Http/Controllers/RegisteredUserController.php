457e75b3964bcdad8edd60b94aaf55437c0b900e
Aleksandar Lazanski <aleksandar.lazanski3@gmail.com>
2025-07-15 23:39:07 +0200

    Basic creating user

--- example-app/app/Http/Controllers/RegisteredUserController.php
+++ example-app/app/Http/Controllers/RegisteredUserController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Validation\Rules\Password;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 
 class RegisteredUserController extends Controller
 {
@@ -13,19 +15,18 @@ class RegisteredUserController extends Controller
 
     public function store(){
        // validate
-       request()->validate([
+      $attributes = request()->validate([
             'first_name' => ['required'],
             'last_name' => ['required'],
             'email' => ['required', 'email', 'max:254'],
             'password' => ['required', Password::min(6), 'confirmed']
        ]);
        // create the user in data base
-
+       $user = User::create($attributes);
        // log in
-
+       Auth::login($user);
        // redirect
-
-       return redirect('/');
+       return redirect('/jobs');
     }
 
 }
