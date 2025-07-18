e195364d04241c92e9fa5b2a36a767bfc8149cea
Aleksandar Lazanski <aleksandar.lazanski3@gmail.com>
2025-07-18 21:47:43 +0200

    Step 4 Can, Cannot

--- example-app/resources/views/jobs/show.blade.php
+++ example-app/resources/views/jobs/show.blade.php
@@ -5,8 +5,10 @@
     <h2>{{ $job->title }}</h2>
     <p>This job pays {{ $job->salary }}</p>
 
+    @can('edit-job', $job)
+        <p class="mt-6">
+            <x-button href="/jobs/{{ $job->id }}/edit">Edit Job</x-button>
+        </p>
+    @endcan
 
-    <p class="mt-6">
-        <x-button href="/jobs/{{ $job->id }}/edit">Edit Job</x-button>
-    </p>
 </x-layout>
