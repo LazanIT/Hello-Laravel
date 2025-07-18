6f704bcf27f9c6ad6ad49bbb4c5b9d660501a5ea
Aleksandar Lazanski <aleksandar.lazanski3@gmail.com>
2025-07-18 22:17:31 +0200

    Step 6 Policy

--- example-app/resources/views/jobs/show.blade.php
+++ example-app/resources/views/jobs/show.blade.php
@@ -5,7 +5,7 @@
     <h2>{{ $job->title }}</h2>
     <p>This job pays {{ $job->salary }}</p>
 
-    @can('edit-job', $job)
+    @can('edit', $job)
         <p class="mt-6">
             <x-button href="/jobs/{{ $job->id }}/edit">Edit Job</x-button>
         </p>
