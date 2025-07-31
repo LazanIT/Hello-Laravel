76c775a0eb0f01ddee0eaca1efce7e384164f0f3
Aleksandar Lazanski <aleksandar.lazanski3@gmail.com>
2025-07-31 23:52:48 +0200

    Transitions

--- pixel-positions/resources/views/components/job-card-wide.blade.php
+++ pixel-positions/resources/views/components/job-card-wide.blade.php
@@ -1,4 +1,5 @@
-<div class="p-4 bg-white/5 rounded-xl flex gap-6">
+<div
+    class="p-4 bg-white/5 rounded-xl flex gap-6 border border-transparent hover:border-blue-800 group cursor-pointer transition-colors duration-300">
     <div>
         <x-employer-logo />
 
@@ -7,7 +8,7 @@
     <div class="flex-1 flex flex-col">
         <a href="#" class="self-start text-sm text-gray-500">Laracast</a>
 
-        <h3 class="font-bold text-xl mt-3">Video Producer</h3>
+        <h3 class="font-bold text-xl mt-3 group-hover:text-blue-800 transition-colors duration-300">Video Producer</h3>
         <p class="text-sm text-gray-400 mt-auto">Full Time - From $60,000</p>
 
     </div>
