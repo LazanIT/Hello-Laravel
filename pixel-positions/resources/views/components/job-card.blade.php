2631f3a54f70764aae625e40ed0703bd7d4551e3
Aleksandar Lazanski <aleksandar.lazanski3@gmail.com>
2025-07-31 23:48:19 +0200

    Hover

--- pixel-positions/resources/views/components/job-card.blade.php
+++ pixel-positions/resources/views/components/job-card.blade.php
@@ -1,8 +1,9 @@
-<div class="p-4 bg-white/5 rounded-xl flex flex-col text-center">
+<div
+    class="p-4 bg-white/5 rounded-xl flex flex-col text-center border border-transparent hover:border-blue-800 group cursor-pointer">
 
     <div class="self-start text-sm">Laracast</div>
     <div class="py-8 font-bold">
-        <h3>Video Producer</h3>
+        <h3 class="group-hover:text-blue-800">Video Producer</h3>
         <p>Full Time - From $60,000</p>
     </div>
     <div class="flex justify-between items-center mt-auto">
