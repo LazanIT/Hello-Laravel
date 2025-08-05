7aed242c6f6cba42adaa1aee3f0ac5e9fd534a74
Aleksandar Lazanski <aleksandar.lazanski3@gmail.com>
2025-08-05 23:38:35 +0200

    Blade and Tailwind Techniques for Laravel Views

--- pixel-positions/resources/views/components/tag.blade.php
+++ pixel-positions/resources/views/components/tag.blade.php
@@ -1,2 +1,14 @@
- <a href="#"
-     class="bg-white/15 px-3 py-1 rounded-xl text-2xs font-bold hover:bg-white/30 transition-colors duration-300">{{ $slot }}</a>
+@props(['size' => 'base'])
+
+@php
+    $classes = 'bg-white/15 px-3 py-1 rounded-xl text-2xs font-bold hover:bg-white/30 transition-colors duration-300';
+
+    if ($size === 'base') {
+        $classes .= ' px-5 py-1 text-sm';
+    }
+
+    if ($size === 'small') {
+        $classes .= ' px-3 py-1 text-2xs';
+    }
+@endphp
+<a href="#" class="{{ $classes }}">{{ $slot }}</a>
